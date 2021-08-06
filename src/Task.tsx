ab6fafac5d366a75a5e7e147f733142248bbd95c
minmeeen <sirapat.mp@gmail.com>
2021-08-06 22:54:17 +0700

    add hover

--- src/Task.tsx
+++ src/Task.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type TaskProps = {
     id: number;
     name: string;
@@ -6,15 +8,27 @@ type TaskProps = {
 }
 
 const Task = ({ id, name, doneFn, deleteFn }: TaskProps) => {
+
+    const [style, setStyle] = useState({ display: 'none' });
+
+
     return (
 
-        <div
-            className="flex justify-between h-8 items-center py-6 border-b"
-        >
+        <div className="flex justify-between h-8 items-center py-6 border-b" 
+            onMouseEnter={e => {
+                setStyle({ display: 'block' });
+            }}
+            onMouseLeave={e => {
+                setStyle({ display: 'none' })
+            }}>
+
             <span className="text-2xl"> {name} </span>
-            <div className="flex space-x-1 items-center">
-                <button className="bg-green-400 w-24 text-2xl" onClick={() => doneFn(id)} >Done</button>
-                <button className="bg-red-400 w-24 text-2xl" onClick={() => deleteFn(id)} >Delete</button>
+            <div className="flex space-x-1 items-center"
+            >
+                <button className="bg-green-400 w-24 text-2xl" style={style}
+                    onClick={() => doneFn(id)} >Done</button>
+                <button className="bg-red-400 w-24 text-2xl" style={style}
+                    onClick={() => deleteFn(id)} >Delete</button>
             </div>
         </div>
 
